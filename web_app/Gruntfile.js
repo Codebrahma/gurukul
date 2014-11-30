22ab0bdc7e0c1bbd2e712de924a689be630568fa
Nithin Krishna <nithin@codebrahma.com>
2014-11-30 20:26:49 +0530

    grunt proxy config setup

--- web_app/Gruntfile.js
+++ web_app/Gruntfile.js
@@ -76,15 +76,16 @@ module.exports = function (grunt) {
         livereload: 35729
       },
       proxies: [{
-        context: '/api',
+        context: ['/api', '/users/sign_in', '/users/sign_out'],
         host: 'localhost',
-        port: 3000
+        port: 3000,
+        https: false,
+        xforward: false
       }],
       livereload: {
         options: {
-          open: true,
-          middleware: function (connect) {
-            return [
+          middleware: function (connect, options) {
+            var middlewares = [
               connect.static('.tmp'),
               connect().use(
                 '/bower_components',
@@ -92,22 +93,51 @@ module.exports = function (grunt) {
               ),
               connect.static(appConfig.app)
             ];
+            if (!Array.isArray(options.base)) {
+              options.base = [options.base];
+            }
+            // Setup the proxy
+            middlewares.push(require('grunt-connect-proxy/lib/utils').proxyRequest);
+            // Serve static files
+            options.base.forEach(function(base) {
+              middlewares.push(connect.static(base));
+            });
+            return middlewares;
           }
         }
       },
       test: {
         options: {
           port: 9001,
-          middleware: function (connect) {
-            return [
-              connect.static('.tmp'),
-              connect.static('test'),
-              connect().use(
-                '/bower_components',
-                connect.static('./bower_components')
-              ),
-              connect.static(appConfig.app)
+          middleware: function (connect, options) {
+            if (!Array.isArray(options.base)) {
+              options.base = [options.base];
+            }
+
+            // Setup the proxy
+            var middlewares = [
+              [
+                connect.static('.tmp'),
+                connect.static('test'),
+                connect().use(
+                  '/bower_components',
+                  connect.static('./bower_components')
+                ),
+                connect.static(appConfig.app)
+              ],
+              require('grunt-connect-proxy/lib/utils').proxyRequest
             ];
+
+            // Serve static files.
+            options.base.forEach(function(base) {
+              middlewares.push(connect.static(base));
+            });
+
+            // Make directory browse-able.
+            var directory = options.directory || options.base[options.base.length - 1];
+            middlewares.push(connect.directory(directory));
+
+            return middlewares;
           }
         }
       },
