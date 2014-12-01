4d9438778daa45d98a909f2f208993d08ffd4cf9
Kameshwaran <kameshwaran1068@yahoo.co.uk>
2014-12-01 22:38:43 +0530

    specs for status models

--- spec/models/test_report_spec.rb
+++ spec/models/test_report_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe TestReport do
   it { should belong_to(:course_test) }
   it { should belong_to(:user_course) }
+  it { should belong_to(:test_report_status) }
 end
--- spec/models/test_report_status_spec.rb
+++ spec/models/test_report_status_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe TestReportStatus do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:test_reports) }
 end
