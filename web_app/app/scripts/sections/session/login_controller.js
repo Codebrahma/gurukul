1554e2271a738a6c688cb7f1978cf8e639f1dd0f
Nithin Krishna <nithin@codebrahma.com>
2014-12-02 01:04:28 +0530

    state handler added to login

--- web_app/app/scripts/sections/session/login_controller.js
+++ web_app/app/scripts/sections/session/login_controller.js
@@ -1,24 +1,27 @@
 angular.module("gu.sections.session")
 
 .controller("gu.sections.session.LoginController",
-  ["$scope", "$window", "gu.data.User",
-  function($scope, $window, User){
+  ["$scope", "$window", "gu.data.User", "gu.util.services.StateHandler",
+  function($scope, $window, User, StateHandler){
 
     function init(){
       $scope.user = {};
       $scope.login = login;
+      $scope.state = StateHandler.getInstance();
     }
 
     function login(){
+      $scope.state.initiate();
       User.login($scope.user).then(loginSuccess, loginFailure);
     };
 
     function loginSuccess(){
-      redirect();
+      $scope.state.success();
+      _.defer(redirect);
     };
 
     function loginFailure(){
-
+      $scope.state.fatal("Unable to login user.");
     };
 
     function redirect(){
