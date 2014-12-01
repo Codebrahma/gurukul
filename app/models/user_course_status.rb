a174d41b48661e1aa6ffd81f5e81cdb9336dcabb
Nithin Krishna <nithin@codebrahma.com>
2014-12-02 02:03:19 +0530

    user course status names, underscore naming convention used

--- app/models/user_course_status.rb
+++ app/models/user_course_status.rb
@@ -3,7 +3,7 @@ class UserCourseStatus < ActiveRecord::Base
   has_many :user_courses
 
   # Constant Column 'status'
-  # Example: UserCourseStatus.ReadyForTest
-  # UserCourseStatus.ReadyForTest.user_courses ( Lists all the user_courses which are ready for test )
+  # Example: UserCourseStatus.ready_for_test
+  # UserCourseStatus.ready_for_test.user_courses ( Lists all the user_courses which are ready for test )
   constantize_column :status
 end
