997eb3dfdc083e4be1aef08963b131bebbf2d6ba
Kameshwaran <kameshwaran1068@yahoo.co.uk>
2014-12-02 22:44:18 +0530

    exception handler included and seralizer method added to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  include ::ApiExceptionHandler
+
+  private
+  def serializer_responder(object_serializer=ApiSuccessSerializer, data={})
+    serializer = (data.is_a?(ActiveRecord::Base) and data.errors.any?) ? ResourceErrorsSerializer : object_serializer
+    render :json => serializer.new(data)
+  end
+
 end
