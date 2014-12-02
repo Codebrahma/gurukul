89b8dd6d014cb3ca3476a263d70b974c1c65bf52
Kameshwaran <kameshwaran1068@yahoo.co.uk>
2014-12-02 22:43:30 +0530

    exception handler to rescue from custom errors

---
+++ app/controllers/concerns/api_exception_handler.rb
@@ -0,0 +1,30 @@
+module ApiExceptionHandler
+  extend ActiveSupport::Concern
+
+  include ActiveSupport::Rescuable
+
+  included do
+    #rescue_from StandardError                , :with => :error_render_method if !Rails.env.development?
+    rescue_from ApiError                     , :with => :api_error_handler
+    rescue_from ActiveRecord::RecordNotFound , :with => :authorization_error_handler
+    rescue_from CanCan::AccessDenied         , :with => :authorization_error_handler
+  end
+
+  private
+
+  def api_error_handler(exception)
+    render :json => exception, :serializer => ApiErrorSerializer
+  end
+
+  def authorization_error_handler(exception)
+    api_error_handler(ApiError.new(9001))
+  end
+
+  def error_render_method(exception)
+    unless Rails.env.test?
+      STDERR.puts "Exception: #{exception.message}"
+      STDERR.puts "Backtrace: #{exception.backtrace.join('\n')}"
+    end
+    api_error_handler(ApiError.new(500))
+  end
+end
