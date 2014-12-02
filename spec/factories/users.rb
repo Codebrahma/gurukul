80ec3db0baffc68f5d2c9bb311d2511f18cd42a0
Kameshwaran <kameshwaran1068@yahoo.co.uk>
2014-12-03 00:36:14 +0530

    factory for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  
+  sequence :email do | n |
+    "user#{n}#{n}#{n}@gmail.com"
+  end
+
+  sequence :name do | n |
+    "user#{n}"
+  end
+
   factory :user do
-    name "MyString"
+    email { generate :email }
+    name  { generate :name }
+    password "password123"
   end
 end
