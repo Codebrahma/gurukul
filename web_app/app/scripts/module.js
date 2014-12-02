aa7aad4a5dd4f519800a296da0c3279d01008d58
Nithin Krishna <nithin@codebrahma.com>
2014-12-03 02:25:21 +0530

    http interceptor to handle AJAX interactions

--- web_app/app/scripts/module.js
+++ web_app/app/scripts/module.js
@@ -10,4 +10,41 @@ angular.module('gu', [
   'ngRoute',
   'ngTouch',
   'LocalStorageModule',
-]);
+])
+
+.config(["$httpProvider", function ($httpProvider) {
+  $httpProvider.interceptors.push(["$window", "$rootScope", "$q", "localStorageService",
+    function ($window, $rootScope, $q, localStorageService) {
+      function isCurrentUserPersisted(){
+        return (localStorageService.get("currentUser") ? true : false);
+      };
+
+      return {
+        response: function(response) {
+          var d = response.data;
+
+          if(!response.data.code){
+            return response;
+          };
+
+          if(d.code == 200){
+            response.data = d.message;
+            return response;
+          } else if(d.code == 400){
+            // SHOW VALIDATION ERRORS
+            $rootScope.$broadcast("kd.events.flash",{
+              type: "danger",
+              message: d.message
+            });
+          } else if(d.code == 5001 && isCurrentUserPersisted()){
+            // SIGNOUT USER
+            localStorageService.clearAll();
+            $window.alert("Your session has expired. Please login to continue.");
+            $window.location.href = "/";
+          };
+
+          return $q.reject(response.data);
+        }
+      };
+  }]);
+}]);
