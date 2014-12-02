fdc913bf61fe5183ee9d9384aa6d5bf6ba35f377
Nithin Krishna <nithin@codebrahma.com>
2014-12-03 02:52:30 +0530

    responding with No active user session if current user session expires

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include ::ApiExceptionHandler
 
+  before_filter :authenticate_user!
+
 private
   def serializer_responder(object_serializer=ApiSuccessSerializer, data={})
     serializer = (data.is_a?(ActiveRecord::Base) and data.errors.any?) ? ResourceErrorsSerializer : object_serializer
@@ -10,5 +12,9 @@ private
 
   def current_ability
     @current_ability ||= Abilities.ability_for(current_user)
+  end
+
+  def authenticate_user!
+    user_signed_in? ? super : raise ApiError.new(ApiError.codes[:NO_ACTIVE_USER_SESSION])
   end
 end
