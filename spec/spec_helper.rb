79df3d8d7458ce8f9b9f4b840922550f28330e80
Nithin Krishna <nithin@codebrahma.com>
2014-12-03 01:40:51 +0530

    spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,4 +55,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.include Requests::JsonHelpers
+  config.include Devise::TestHelpers, type: :controller
 end
