ebcf57a409a213a13a00dc64978229b41931de02
Nithin Krishna <nithin@codebrahma.com>
2014-12-03 02:55:00 +0530

    sessions controller authorizations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < Devise::SessionsController
+  skip_before_filter :require_no_authentication, :only => :create
+  skip_before_filter :verify_signed_out_user, :only => :destroy
 
-  before_filter :load_and_authorize_resource, :only => [ :create ]
+  before_filter :load_and_authorize_resource, :only => :create
+  before_filter :authorize_destroy, :only => :destroy
 
   def create
     sign_in(resource_name, resource)
@@ -12,9 +15,13 @@ class SessionsController < Devise::SessionsController
     serializer_responder ApiSuccessSerializer, true
   end
 
-  private
+private
   def load_and_authorize_resource
     self.resource = warden.authenticate(auth_options)
     raise ApiError.new(ApiError.codes[:INVALID_LOGIN_CREDENTIALS]) if resource.nil?
+  end
+
+  def authorize_destroy
+    raise ApiError.new(5001) if !current_user.present?
   end
 end
